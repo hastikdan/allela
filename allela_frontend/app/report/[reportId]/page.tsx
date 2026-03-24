abbeb9ba15b75ab03e30536cc6172ae19eaebbc9
Daniel <hastikdan@gmail.com>
2026-03-24 06:51:16 +0100

    fix: add Logo import to report page

--- allela_frontend/app/report/[reportId]/page.tsx
+++ allela_frontend/app/report/[reportId]/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams, useSearchParams } from "next/navigation";
+import Logo from "../../components/Logo";
 
 const API = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
